c0f2e960a5c904cd24e7ae22760a0dc1aeff4993
GuyKopel1568 <guykopel1568@gmail.com>
2025-02-21 16:47:26 +0200

    Add css modules and global css

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Product from "./pages/Product";
 import HomePage from "./pages/HomePage";
 import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="app" element={<AppLayout />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="*" element={<PageNotFound />} />
