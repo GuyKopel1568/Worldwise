eecf85128585fe17d4841afeeab18351694eb242
GuyKopel1568 <guykopel1568@gmail.com>
2025-02-23 14:10:41 +0200

    Add fake API- to do these we nedd to add npm i json-server and add to the package.json ito scripts server

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,16 @@ import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
+import CityList from "./components/CityList";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route index element={<HomePage />} />
         <Route path="app" element={<AppLayout />}>
-          <Route path="cities" element={<p>List of cities</p>} />
+          <Route index element={<CityList />} />
+          <Route path="cities" element={<CityList />} />
           <Route path="countries" element={<p>List of countries</p>} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
